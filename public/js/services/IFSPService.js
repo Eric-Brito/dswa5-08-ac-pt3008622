232260c97a7b524a8634ec584e823b13cd74178a
Eric-Brito <101842271+Eric-Brito@users.noreply.github.com>
2022-04-07 19:24:17 -0300

    Update IFSPService.js

--- public/js/services/IFSPService.js
+++ public/js/services/IFSPService.js
@@ -1,4 +1,7 @@
 angular.module('ifsp').factory('Contato', 'Curso', function($resource) {
     return $resource('/contatos/:id');
+});
+
+angular.module('ifsp').factory('Curso', function($resource) {
     return $resource('/cursos/:id');
 });
